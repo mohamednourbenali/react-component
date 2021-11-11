a0db0f5115b9d0b1e27a78abff09c99c6fb4d0ca
mohamednourbenali <mohamednourbenali@gmail.com>
2021-11-11 21:29:19 +0100

    checkpoint react component

---
+++ src/component/profile/Adress.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Adress = () => {
+    return (
+        <div>
+            <p>06 rue 4113 cité ezzouhour 1 , tunis</p>
+        </div>
+    )
+}
+
+export default Adress
---
+++ src/component/profile/ProfilePhoto.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import photo from './project.jpg'
+const ProfilePhoto = () => {
+    return (
+        <div>
+            <img src={photo}/>
+        </div>
+    )
+}
+
+export default ProfilePhoto
